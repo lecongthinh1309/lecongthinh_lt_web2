55fd82a75ff8ee83cf2bf24494de10086c3cafd0
Quangha Ha <lecongthinh2005@gmail.com>
2026-06-05 08:37:54 +0700

    first commit

---
+++ src/main/java/com/lecongthinh/lt_web2/LtWeb2Application.java
@@ -0,0 +1,13 @@
+package com.lecongthinh.lt_web2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtWeb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lecongthinh/lt_web2/LtWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecongthinh.lt_web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
